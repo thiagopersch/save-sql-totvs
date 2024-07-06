8bd1f83090d77696f8d5e5c9c1546bc2e28c5577
Tiago Persch <thiagopersch@gmail.com>
2024-07-06 18:39:20 -0400

    fix: corrigido url do cors e adicionado .env.exemple

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin:
+      process.env.APP_ENV === 'production'
+        ? process.env.URL_CLIENT
+        : 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
