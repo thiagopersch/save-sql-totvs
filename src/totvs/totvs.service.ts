c8fb8087bde098bd28ef4089d6b905ac84a97848
Tiago Persch <thiagopersch@gmail.com>
2024-07-28 17:31:26 -0400

    feat: create request getSchema with dataservers

--- src/totvs/totvs.service.ts
+++ src/totvs/totvs.service.ts
@@ -4,6 +4,88 @@ import { parseStringPromise } from 'xml2js';
 
 @Injectable()
 export class TotvsService {
+  async getSchema(
+    dataServerName: string,
+    username: string,
+    password: string,
+    tbc: string,
+    contexto: string,
+  ) {
+    try {
+      const existTbc = tbc.endsWith('/')
+        ? `${tbc}wsDataServer/MEX?wsdl`
+        : `${tbc}/wsDataServer/MEX?wsdl`;
+
+      const client = await soap.createClientAsync(existTbc);
+      const auth =
+        'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+      client.addHttpHeader('Authorization', auth);
+
+      const args = {
+        DataServerName: dataServerName,
+        Contexto: contexto,
+      };
+
+      const [result] = await client.GetSchemaAsync(args);
+      const parsedResult = await parseStringPromise(result.GetSchemaResult);
+
+      let dynamicKey = dataServerName;
+
+      if (!parsedResult[dynamicKey]) {
+        const shouldRemoveDataSuffix =
+          (dataServerName.endsWith('Data') ||
+            dataServerName.endsWith('DataBR')) &&
+          (!dataServerName.endsWith('Data') ||
+            !dataServerName.endsWith('DataBR'));
+        if (shouldRemoveDataSuffix) {
+          dynamicKey = dataServerName.replace('Data', '').replace('BR', '');
+        }
+      }
+
+      if (!parsedResult[dynamicKey]) {
+        throw new Error(
+          `Chave ${dynamicKey} não encontrada no resultado analisado.`,
+        );
+      }
+
+      const tables =
+        parsedResult[dynamicKey]['xs:schema'][0]['xs:element'][0][
+          'xs:complexType'
+        ][0]['xs:choice'][0]['xs:element'];
+
+      const extractedData = tables.map((table: any) => {
+        const tableName = table.$.name;
+        const schemaElements =
+          table['xs:complexType'][0]['xs:sequence'][0]['xs:element'];
+
+        const fields = schemaElements.map((element: any) => ({
+          name: element.$.name,
+          caption: element.$['msdata:Caption'],
+          type: (element.$.type ?? 'xs:string').replace(/^xs:/, ''),
+          default: element.$.default,
+        }));
+
+        return {
+          tableName,
+          fields,
+        };
+      });
+
+      const primaryKey =
+        parsedResult[dynamicKey]['xs:schema'][0]['xs:element'][0][
+          'xs:unique'
+        ][0]['xs:field'];
+
+      const primaryKeyFields = primaryKey
+        .map((field: any) => field.$.xpath.replace(/^mstns:/, ''))
+        .join(';');
+
+      return { extractedData, primaryKeys: primaryKeyFields };
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
   async readView(
     dataServerName: string,
     filtro: string,
